b7c766bde65467dd054a1667ded58885cad296f6
str4d <str4d@mail.i2p>
2014-08-08 01:39:04 +0000

    Use pow from Python-Future

--- i2p/i2cp/crypto.py
+++ i2p/i2cp/crypto.py
@@ -4,6 +4,7 @@ from __future__ import division
 from __future__ import absolute_import
 from future.builtins import int
 from future.builtins import open
+from future.builtins import pow
 from future import standard_library
 standard_library.install_hooks()
 
