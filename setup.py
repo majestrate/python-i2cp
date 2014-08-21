b7d6399b107ba334ead0e8b6803b74635d414fb9
jeff <ampernand@gmail.com>
2014-08-21 07:27:13 -0400

    fix setup.py, ammend .gitignore for pycharm

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 
-#with open('README.rst', 'r') as infile:
-#    long_description = infile.read()
+with open('README.rst', 'r') as infile:
+    long_description = infile.read()
 
-#with open('requirements.txt', 'r') as infile:
-#    install_requires = infile.read().split()
+with open('requirements.txt', 'r') as infile:
+    install_requires = infile.read().split()
 
 setup(
     name='i2p.i2cp',
     description='I2CP client library for I2P',
-    #long_description=long_description,
+    long_description=long_description,
     author='Jeff',
     author_email='ampernand@gmail.com',
     url='https://github.com/majestrate/python-i2cp',
@@ -28,11 +28,10 @@ setup(
         'Topic :: Internet',
     ],
     license='Public Domain',
-    version='0.0.0',
-    #setup_requires=['vcversioner>=1'],
-    #vcversioner={
-    #    'version_module_paths': ['i2p/i2cp/_version.py'],
-    #},
-    #install_requires=install_requires,
+    setup_requires=['vcversioner>=1'],
+    vcversioner={
+        'version_module_paths': ['i2p/i2cp/_version.py'],
+    },
+    install_requires=install_requires,
     packages=['i2p.i2cp', 'i2p.i2cp.test', 'i2p'],
 )
