cd54ecc05ea096d323ee671999877b6cae66696c
jeff <ampernand@gmail.com>
2015-05-16 11:51:23 -0400

    add i2p.socket test cases

---
+++ src/i2p/socket/test/test_socket.py
@@ -0,0 +1,27 @@
+from i2p import socket
+
+from unittest import TestCase
+
+class TestSocket(TestCase):
+
+    def setUp(self):
+        # set up i2p socket module
+        socket.setup(("10.0.3.1", 7654))
+        
+    def test_connect(self):
+        """
+        test socket connections
+        """
+        sock = socket.socket()
+        assert not sock.isConnected()
+        sock.connect(("psi.i2p", 80))
+        assert sock.isConnected()
+        sock.close()
+        asser not sock.isConnected()
+
+    def test_bind(self):
+        """
+        TODO: implement
+        """
+        assert False
+        
