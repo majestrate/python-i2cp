e6863944cd1e206b88a7573647382510f0d2cdda
jeff <ampernand@gmail.com>
2015-07-24 17:06:34 -0400

    i2cp.Connection.open is a coroutine

--- src/i2p/tun/__main__.py
+++ src/i2p/tun/__main__.py
@@ -139,7 +139,7 @@ def main():
 
     opts = {'inbound.length':'%d' % args.hops, 'outbound.length' :'%d' % args.hops}
     conn = i2cp.Connection(handler, i2cp_host=i2cp_host, i2cp_port=i2cp_port, keyfile=args.keyfile, evloop=loop, session_options=opts)
-    conn.open()
+    loop.run_until_complete(conn.open())
     loop.call_soon(_wait_for_done, conn, 1.0)
     loop.run_until_complete(ftr)
     
