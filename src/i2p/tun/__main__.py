02f55534b072f6ae0d20fa756c44eb39e0e55e61
jeff <ampernand@gmail.com>
2015-07-25 13:38:04 -0400

    use more outbound tunnels

--- src/i2p/tun/__main__.py
+++ src/i2p/tun/__main__.py
@@ -139,6 +139,8 @@ def main():
         handler = Handler(args.remote, tun, lambda x : x)
 
     opts = {'inbound.length':'%d' % args.hops, 'outbound.length' :'%d' % args.hops}
+    opts['outbound.quantity'] = '8'
+    opts['inbound.quntity'] = '2'
     conn = i2cp.Connection(handler, i2cp_host=i2cp_host, i2cp_port=i2cp_port, keyfile=args.keyfile, loop=loop, session_options=opts)
     loop.run_until_complete(conn.open())
     try:
